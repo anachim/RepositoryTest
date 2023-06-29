8f0d6563d431f8fe1719963320db0278062d7b98
Anachim <anachim.skyworker@gmail.com>
2023-06-29 12:09:12 +0200

    Initial Commit

---
+++ NUnitTesting/UnitTesting.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace NUnitTesting;
+
+public class UnitTesting
+{
+    private int _valoreIniziale;
+
+    [SetUp]
+    public void Init()
+    {
+        _valoreIniziale = 42;
+    }
+
+    [Test]
+    public void SimpleTest()
+    {
+        _valoreIniziale.Should().BeGreaterThan(0);
+    }
+
+    [TestCase(1, true)]
+    [TestCase(2, true)]
+    [TestCase(-44, false)]
+    public void MultipleTest(int value, bool expected)
+    {
+        var actual = _valoreIniziale + value;
+
+        if (expected)
+        {
+            actual.Should().BePositive();
+        }
+        else
+        {
+            actual.Should().BeNegative();
+        }
+    }
+}
---
+++ XUnitTesting/UnitTesting.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace XUnitTesting;
+
+public class UnitTesting
+{
+    private int _valoreIniziale;
+
+    public UnitTesting()
+    {
+        _valoreIniziale = 42;
+    }
+
+    [Fact]
+    public void SimpleTest()
+    {
+        _valoreIniziale.Should().BeGreaterThan(0);
+    }
+
+    [Theory]
+    [InlineData(1, true)]
+    [InlineData(2, true)]
+    [InlineData(-44, false)]
+    public void MultipleTest(int value, bool expected)
+    {
+        var actual = _valoreIniziale + value;
+
+        if (expected)
+        {
+            actual.Should().BePositive();
+        }
+        else
+        {
+            actual.Should().BeNegative();
+        }
+    }
+}
